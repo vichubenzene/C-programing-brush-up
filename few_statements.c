a658b0905786dae877ca3df183d4bbf95129a31e
vichubenzene <vichusathappan@gmail.com>
2022-06-28 19:16:40 +0530

    19:17
    romba borreng

--- few_statements.c
+++ few_statements.c
@@ -4,5 +4,45 @@ int main() {
   if (20 > 18) {
     printf("20 is greater than 18");
   }  
+  int f;
+  int s;
+  printf("Enter the first number:");
+  scanf("%d",&f);
+  printf("Enter the second number:");
+  scanf("%d",&s);
+  if (s>f){
+  	printf ("%d is gretat that %d",s,f );}
+  else{
+  	printf ("%d is less r equal that %d",s,f ) ;}
+  	
+  int time = 20;
+  (time == 20) ? printf("Good day.") : printf("Good evening.");
+int day = 4;
+
+switch (day) {
+  case 1:
+    printf("Monday");
+    break;
+  case 2:
+    printf("Tuesday");
+    break;
+  case 3:
+    printf("Wednesday");
+    break;
+  case 4:
+    printf("Thursday");
+    break;
+  case 5:
+    printf("Friday");
+    break;
+  case 6:
+    printf("Saturday");
+    break;
+  case 7:
+    printf("Sunday");
+    break;
+  default:
+    printf("Looking forward to the Weekend");
+}
   return 0;
 }
---
+++ few_statements2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+  int i = 0;
+  
+  while (i < 5) {
+    printf("%d\n", i);
+    i++;
+  }
+  
+int i = 0;
+
+do {
+  printf("%d\n", i);
+  i++;
+}
+while (i < 5);
+  int n;
+for (n=0;n<4;n++){
+	printf("%d",n);
+}
+
+int i;
+
+for (i = 0; i < 10; i++) {
+  if (i == 4) {
+    break;
+  }
+  printf("%d\n", i);
+}
+return 0
+}
